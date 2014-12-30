8f4ddb3c78b80ab28ff34ed9df774ae74eafd075
Ioannis Papadopoulos <ioannis.papadopoulos@gmail.com>
2014-12-30 23:34:16 +0100

    First complete versions of Segment and Trip classes

--- python/telematics/Segment.py
+++ python/telematics/Segment.py
@@ -1,12 +1,55 @@
+import numpy
+
 # Segment class
 class Segment:
     def __init__(self, coordinates ):
         self.__data = coordinates
+        self.__v = numpy.diff( coordinates, axis=0)
         return
 
     def rawData(self):
         return self.__data
 
-    def size(self):
-        return len(self.__data)
+    # The time duration of the trip
+    def duration( self ):
+        return len( self.__data ) - 1
     
+    # The distance (and speed) vector
+    def speedValues( self ):
+        return numpy.apply_along_axis( lambda x: numpy.sqrt( x[0]**2+x[1]**2), 1, self.__v )
+
+    # The distance traveled
+    def distanceTraveled( self ):
+        return numpy.sum( self.speedValues() )
+
+    # The acceleration values
+    def accelerationValues( self ):
+        speedValues = self.speedValues()
+        return numpy.diff( speedValues )
+
+    # The angular values
+    def angularValues( self ):
+        angles = numpy.zeros( len(self.__v) - 1 )
+        for i in range(len(angles)):
+            v1 = self.__v[i]
+            v2 = self.__v[i+1]
+            mv1 = numpy.sqrt( v1[0]**2 + v1[1]**2)
+            if mv1 == 0:
+                continue
+            mv2 = numpy.sqrt( v2[0]**2 + v2[1]**2)
+            if mv2 == 0:
+                continue
+            mv = mv1 * mv2
+            sint = ( v1[0]*v2[1] - v1[1]*v2[0] ) / mv
+            cost = ( v1[0]*v2[0] + v1[0]*v2[0] ) / mv
+            if sint > 1:
+                sint = 1
+            if sint < -1:
+                sint = -1
+            theta = numpy.arcsin( sint )
+            if cost >= 0:
+                angles[i] = theta
+            else:
+                angles[i] = numpy.pi - theta
+        return angles
+            
--- python/telematics/Trip.py
+++ python/telematics/Trip.py
@@ -14,52 +14,94 @@ class Trip:
     def readFromCSV( self, fileName ):
         self.__rawData = numpy.genfromtxt( fileName, dtype=float, delimiter=',', skip_header = 1 )
         # The velocity vectors
-        self.__v = numpy.diff( self.__rawData, axis=0)
         self.__generateSegments()
         return
-
-    # Size of the trip data
-    def size( self ):
-        return len( self.__rawData )
 
     # Returns the actual data
     def rawData( self ):
         return self.__rawData
 
     # The time duration of the trip
-    def duration( self ):
+    def tripDuration( self ):
         return len( self.__rawData ) - 1
 
-    # The destination distance
-    def destinationDistance( self ):
-        (x,y) = self.__rawData[ len(self.__rawData) - 1]
-        return numpy.sqrt( x**2 + y**2)
+    # The trip segments
+    def segments( self ):
+        return self.__segments
 
-    # The average speed along a straight line
-    def speedAlongStraightLine( self ):
-        return self.destinationDistance() / self.duration()
+    # The total distance traveled, derived from the segments
+    def distanceTraveled( self ):
+        result = 0
+        for segment in self.__segments:
+            result += segment.distanceTraveled()
+        return result
 
-    # The distance (and speed) vector
-    def speedSegments( self ):
-        return numpy.apply_along_axis( lambda x: numpy.sqrt( x[0]**2+x[1]**2), 1, self.__v )
-
-    # The distance traveled
-    def travelDistance( self ):
-        return numpy.sum( self.speedSegments() )
-
-    # The mean traveling speed
+    # The mean spead derived by the segments
     def meanSpeed( self ):
-        return self.travelDistance() / self.size()
+        dS = 0
+        dt = 0
+        for segment in self.__segments:
+            dS += segment.distanceTraveled()
+            dt += segment.duration()
+        if dt == 0:
+            return 0
+        else:
+            return dS/dt
 
-    # The travel to distance
-    def travelToDistance( self ):
-        return self.travelDistance() / self.destinationDistance()
+    # The speed values
+    def speedValues( self ):
+        speedValues = numpy.array([])
+        for segment in self.__segments:
+            speedValues = numpy.hstack( (speedValues, segment.speedValues() ) )
+        return speedValues
+
+    # The speed values distribution
+    def speedStatistics( self ):
+        speedValues = self.speedValues()    
+        if len( speedValues ) < 3:
+            return (0,0,0,0,0)
+        else:
+            return numpy.percentile(speedValues,[5,25,50,75,95])
+
+    # The acceleration values
+    def accelerationValues( self ):
+        accelerationValues = numpy.array([])
+        for segment in self.__segments:
+            accelerationValues = numpy.hstack( (accelerationValues, segment.accelerationValues() ) )
+        return accelerationValues
+
+    # The acceleration values distribution
+    def accelerationStatistics( self ):
+        accelerationValues = self.accelerationValues()
+        if len( accelerationValues ) < 3:
+            return (0,0,0,0,0)
+        else:
+            return numpy.percentile(accelerationValues,[5,25,50,75,95])
+
+    # The direction angles
+    def angularValues( self ):
+        angularValues = numpy.array([])
+        for segment in self.__segments:
+            angularValues = numpy.hstack( (angularValues, segment.angularValues() ) )
+        return angularValues
+        
+
+    # The direction angles distribution
+    def angularStatistics( self ):
+        angularValues = self.angularValues()
+        if len( angularValues ) < 3:
+            return (0,0,0,0,0)
+        else:
+            return numpy.percentile(angularValues,[5,25,50,75,95])
+        
+
+    # Fourier transformation of some quantity (velocity, angle)
 
     # Private function that generates the trip segments
     def __generateSegments(self):
         
-        zeroSpeedTolerance = 1  # The speed below which it is considered that the speed is really zero
-        zeroSpeedMaxCounts = 10 # After 10 seconds we mark a new segment
+        zeroSpeedTolerance = 1.5  # The speed below which it is considered that the speed is really zero
+        zeroSpeedMaxCounts = 2 # After 2 seconds we mark a new segment
         maxSpeed = 60 # The maximum speed allowed in a segment
         maxAcceleration = 10 # The maximum acceleration allowed in a segment
 
@@ -67,10 +109,11 @@ class Trip:
         i = 2
         segmentStartIndex = 0
         zeroSpeedCounter = 0
+        v = numpy.diff( self.__rawData, axis=0)
         
         while i < len(self.__rawData):
-            currentSpeed = self.__v[i-1]
-            currentAcceleration = currentSpeed - self.__v[i-2]
+            currentSpeed = v[i-1]
+            currentAcceleration = currentSpeed - v[i-2]
 
             # Convert vectors to magnitudes
             currentSpeed = numpy.sqrt( currentSpeed[0]**2 + currentSpeed[1]**2)
@@ -80,27 +123,22 @@ class Trip:
             if currentSpeed < zeroSpeedTolerance:
                 zeroSpeedCounter += 1
 
-            # Mark the end of the segment
-            if zeroSpeedCounter >= zeroSpeedMaxCounts:
+            # Check if it comes right after a 0 speed segment
+            # Check for abnormal speed
+            # Check for abnormal acceleration
+            if (zeroSpeedCounter >= zeroSpeedMaxCounts) or ( currentSpeed > maxSpeed ) or ( currentAcceleration > maxAcceleration ):
+                
+                # Mark the end of the segment, including up to the previous point,
+                # as long as it has more than two points
+                # Removing also the trailing zero-speed segments
+                
                 lastIndex = i - zeroSpeedCounter
                 if lastIndex - segmentStartIndex > 2:
                     segment = Segment( self.__rawData[ segmentStartIndex : lastIndex ] )
                     self.__segments.append( segment )
-                    
-                # Reinitiliase the conditions using the next point as the initial point.
-                segmentStartIndex = i
-                i += 2
-                continue
-                
-
-            # Check for abnormaly high speed or accelaration
-            if ( currentSpeed > maxSpeed ) or ( currentAcceleration > maxAcceleration ):
-                # Mark the end of the segment, including up to the previous point, as long as it has more than two points
-                if i - segmentStartIndex > 2:
-                    segment = Segment( self.__rawData[ segmentStartIndex : i-1 ] )
-                    self.__segments.append( segment )
                 
                 # Reinitiliase the conditions using the next point as the initial point.
+                zeroSpeedCounter = 0
                 segmentStartIndex = i
                 i += 2
                 continue
@@ -108,6 +146,7 @@ class Trip:
             i += 1 # Move to the next point in the trip
 
         # Treat the remaining trip as a segment
+        i -= zeroSpeedCounter
         if i - segmentStartIndex > 2:
             segment = Segment( self.__rawData[ segmentStartIndex : i-1 ] )
             self.__segments.append( segment )
