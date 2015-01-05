d6261e0aff6d4f951dc20da48b36d5347f495e0a
Ioannis Papadopoulos <ioannis.papadopoulos@gmail.com>
2015-01-05 23:17:28 +0100

    better memory management. Raw data not needed once segments have been created

--- python/telematics/Trip.py
+++ python/telematics/Trip.py
@@ -6,9 +6,11 @@ class Trip:
     
     # Constructor
     def __init__( self, tripId ):
-        self.__rawData = []
+        self.__lastPoint = None
+        self.__tripDuration = 0
         self.__segments = []
         self.__id = tripId
+        self.__segmentsGenerated = False
         return
 
     def __del__( self ):
@@ -17,21 +19,17 @@ class Trip:
     # Returns the trip id
     def id( self ):
         return self.__id
-    
-    # Reading the input from a CSV file
-    def readFromCSV( self, fileName ):
-        self.__rawData = numpy.genfromtxt( fileName, dtype=float, delimiter=',', skip_header = 1 )
-        # The velocity vectors
-        self.__generateSegments()
-        return
 
-    # Returns the actual data
-    def rawData( self ):
-        return self.__rawData
+    # Sets directly the raw data from a numpy array
+    def setData( self, data ):
+        self.__lastPoint = data[ len(data)-1]
+        self.__tripDuration = len(data)-1
+        self.__generateSegments( data )
+        return
 
     # The time duration of the trip
     def tripDuration( self ):
-        return len( self.__rawData ) - 1
+        return self.__tripDuration
 
     # The trip segments
     def segments( self ):
@@ -40,6 +38,10 @@ class Trip:
     # Returns the number of segments
     def numberOfSegments( self ):
         return len(self.__segments)
+
+    # Returns the distance to the ending point
+    def distanceToEndPoint( self ):
+        return numpy.sqrt( self.__endPoint[0]**2 + self.__endPoint[1]**2 )
 
     # The total distance traveled, derived from the segments
     def distanceTraveled( self ):
@@ -69,9 +71,8 @@ class Trip:
 
     # The speed values distribution
     def speedStatistics( self ):
-        speedValues = self.speedValues()    
         if len( speedValues ) < 3:
-            return (0,0,0,0,0)
+            return numpy.zeros(5)
         else:
             return numpy.percentile(speedValues,[5,25,50,75,95])
 
@@ -86,7 +87,7 @@ class Trip:
     def accelerationStatistics( self ):
         accelerationValues = self.accelerationValues()
         if len( accelerationValues ) < 3:
-            return (0,0,0,0,0)
+            return numpy.zeros(5)
         else:
             return numpy.percentile(accelerationValues,[5,25,50,75,95])
 
@@ -102,29 +103,26 @@ class Trip:
     def angularStatistics( self ):
         angularValues = self.angularValues()
         if len( angularValues ) < 3:
-            return (0,0,0,0,0)
+            return numpy.zeros(5)
         else:
-            return numpy.percentile(angularValues,[5,25,50,75,95])
-        
-    # Fourier transformation of speed
-
-    # Fourier transformation of angles
+            return numpy.percentile(angularValues,[5,25,50,75,95])        
 
     # Private function that generates the trip segments
-    def __generateSegments(self):
+    def __generateSegments(self, rawData ):
+        if self.__segmentsGenerated :
+            return
         
         zeroSpeedTolerance = 1.5  # The speed below which it is considered that the speed is really zero
         zeroSpeedMaxCounts = 2 # After 2 seconds we mark a new segment
         maxSpeed = 60 # The maximum speed allowed in a segment
         maxAcceleration = 10 # The maximum acceleration allowed in a segment
 
-        self.__segments = []
         i = 2
         segmentStartIndex = 0
         zeroSpeedCounter = 0
-        v = numpy.diff( self.__rawData, axis=0)
+        v = numpy.diff( rawData, axis=0)
         
-        while i < len(self.__rawData):
+        while i < len(rawData):
             currentSpeed = v[i-1]
             currentAcceleration = currentSpeed - v[i-2]
 
@@ -147,7 +145,7 @@ class Trip:
                 
                 lastIndex = i - zeroSpeedCounter
                 if lastIndex - segmentStartIndex > 2:
-                    segment = Segment( self.__rawData[ segmentStartIndex : lastIndex ] )
+                    segment = Segment( rawData[ segmentStartIndex : lastIndex ] )
                     self.__segments.append( segment )
                 
                 # Reinitiliase the conditions using the next point as the initial point.
@@ -161,9 +159,11 @@ class Trip:
         # Treat the remaining trip as a segment
         i -= zeroSpeedCounter
         if i - segmentStartIndex > 2:
-            segment = Segment( self.__rawData[ segmentStartIndex : i-1 ] )
+            segment = Segment( rawData[ segmentStartIndex : i-1 ] )
             self.__segments.append( segment )
-        
+
+        # Set the flag
+        self.__segmentsGegenerated = True
         return
         
 
