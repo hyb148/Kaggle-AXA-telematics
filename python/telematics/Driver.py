a4c259b1f8e1fde58099cd6f0045ba1beae9b233
Ioannis Papadopoulos <ioannis.papadopoulos@gmail.com>
2015-01-05 23:18:46 +0100

    Driver data read from compressed (pickle) file to improve performance

--- python/telematics/Driver.py
+++ python/telematics/Driver.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import numpy
+import pickle
 from .Trip import Trip
 
 # Driver class
@@ -7,6 +9,7 @@ class Driver:
     def __init__( self, driverId ):
         self.__id = driverId
         self.__trips = {}
+        self.__zeroSegmentTrips = 0
         return
 
     def __del__( self ):
@@ -22,16 +25,19 @@ class Driver:
             self.__trips[i] = None
             del o
         self.__trips = {}
+        self.__zeroSegmentTrips = 0
 
     def readTripsFromDirectory( self, directoryName ):
         self.__clearTrips()
-        tripFiles = os.listdir( directoryName )
-        for tripFile in tripFiles:
-            tripId = int(tripFile.split('.')[0])
-            fileName = os.path.join( directoryName, tripFile )
-            trip = Trip( tripId )
-            trip.readFromCSV( fileName )
+        tripFile = os.path.join( directoryName, str(self.__id) + ".data" )
+        file = open(tripFile, 'rb')
+        while file.read(1):
+            file.seek(-1,1)
+            (tripId, tripData) = pickle.load( file )
+            trip = Trip(tripId)
+            trip.setData( tripData )
             self.__trips[tripId] = trip
+        file.close()
         return
 
     def numberOfTrips( self ):
@@ -39,3 +45,52 @@ class Driver:
 
     def getTrip( self, tripId ):
         return self.__trips[ tripId ]
+
+    # Returns the number of zero segment trips
+    def zeroSegmentTrips( self ):
+        return self.__zeroSegmentTrips
+
+    # Returns the total number of trips
+    def numberOfTrips( self ):
+        return len( self.__trips )
+
+    # Collects the speed statistics
+    def speedStatistics( self ):
+        quantiles = numpy.zeros([(self.numberOfTrips() - self.zeroSegmentTrips() ), 5] )
+        j = 0
+        for i in self.__trips:
+            trip = self.__trips[i]
+            if trip.numberOfSegments() == 0:
+                continue
+            else:
+                quantiles[j] = trip.speedStatistics()
+                j += 1        
+        return numpy.array([( numpy.mean( quantiles[:,k]), numpy.std( quantiles[:,k]) ) for k in range(5)]).reshape(10)
+
+    # Collects the acceleration statistics
+    def accelerationStatistics( self ):
+        quantiles = numpy.zeros([(self.numberOfTrips() - self.zeroSegmentTrips() ), 5] )
+        j = 0
+        for i in self.__trips:
+            trip = self.__trips[i]
+            if trip.numberOfSegments() == 0:
+                continue
+            else:
+                quantiles[j] = trip.accelerationStatistics()
+                j += 1        
+        return numpy.array([( numpy.mean( quantiles[:,k]), numpy.std( quantiles[:,k]) ) for k in range(5)]).reshape(10)
+
+    # Collects the acceleration statistics
+    def angularStatistics( self ):
+        quantiles = numpy.zeros([(self.numberOfTrips() - self.zeroSegmentTrips() ), 5] )
+        j = 0
+        for i in self.__trips:
+            trip = self.__trips[i]
+            if trip.numberOfSegments() == 0:
+                continue
+            else:
+                quantiles[j] = trip.angularStatistics()
+                j += 1        
+        return numpy.array([( numpy.mean( quantiles[:,k]), numpy.std( quantiles[:,k]) ) for k in range(5)]).reshape(10)
+
+    
