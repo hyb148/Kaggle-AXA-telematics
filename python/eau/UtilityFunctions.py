b00935dd25670ced1dbbc959abf04238c15075d2
Ioannis Papadopoulos <ioannis.papadopoulos@gmail.com>
2015-01-06 13:11:02 +0100

    added functions to operate on angles and rotation matrices

--- python/eau/UtilityFunctions.py
+++ python/eau/UtilityFunctions.py
@@ -9,8 +9,8 @@ def areaUnderLine( x, y ):
 
     return result
 
-# Gets the angle from y to x
-def angleOfVectors( x, y ):
+# Gets the angle from y to x as rotation matrix
+def angleOfVectorsAsRotationMatrix( x, y ):
     mx = numpy.sqrt(x[0]**2 + x[1]**2)
     if mx == 0:
         return numpy.array( [ [ 1, 0 ],
@@ -25,6 +25,32 @@ def angleOfVectors( x, y ):
     return numpy.array( [ [ costheta, -sintheta ],
                           [ sintheta, costheta ] ] )
 
-# Rotates x by theta
-def rotateVector( x, rotationMatrix ):
+# Gets the angle from y to x
+def angleOfVectors( x, y ):
+    mx = numpy.sqrt(x[0]**2 + x[1]**2)
+    if mx == 0:
+        return 0
+    my = numpy.sqrt(y[0]**2 + y[1]**2)
+    if my == 0:
+        return 0
+    mxy = mx * my
+    sint = ( x[0]*y[1] - x[1]*y[0] ) / mxy
+    cost = ( x[0]*y[0] + x[1]*y[1] ) / mxy
+    if sint > 1: sint = 1
+    if sint < -1: sint = -1
+    if cost >= 0: return numpy.arcsin( sint )
+    else: return numpy.pi - numpy.arcsin( sint )
+    
+
+
+# Rotates x by a rotation matrix
+def rotateVectorByMatrix( x, rotationMatrix ):
     return x.dot( rotationMatrix )
+
+
+def rotateVector( x, theta ):
+    cost = numpy.cos(theta)
+    sint = numpy.sin(theta)
+    R = numpy.array( [ [cost, -sint],
+                       [sint, cost] ] )
+    return rotateVectorByMatrix( x, R )
