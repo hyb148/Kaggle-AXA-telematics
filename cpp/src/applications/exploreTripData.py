895f68814ecf679cb3e453e1cdf0a993474c4acc
Ioannis Papadopoulos <ioannis.papadopoulos@gmail.com>
2015-01-16 13:11:00 +0100

    first entry

--- cpp/src/applications/exploreTripData.py
+++ cpp/src/applications/exploreTripData.py
@@ -5,8 +5,6 @@ import os
 import os.path
 
 import matplotlib.pyplot as plt
-        
-
 
 class DataReader:
     def __init__( self, cppToPythonPipeName, pythonToCppPipeName ):
@@ -143,32 +141,96 @@ dataReader = DataReader('cpptopythonpipe','pythontocpppipe')
 drivers = dataReader.driverList()
 
 #driverId = drivers[0]
-driverId = 1
+driverId = 2
 
 tripIds = dataReader.tripList( driverId )
 
 
 # The Raw Data Window
-fig1 = plt.figure(num=1, figsize=(7,10.5), facecolor='lightblue')
-plt.get_current_fig_manager().set_window_title("Raw Data")
+fig = plt.figure(num=1, figsize=(20,10.5), facecolor='lightblue')
+plt.get_current_fig_manager().set_window_title("Trip summary")
 
 # Looping over the trips
 for tripId in tripIds:
-#    if tripId not in (116,126,131,167,200,30,32,46,60,81): continue
     print( "Processing trip " + str(tripId) )
 
     travelDuration = dataReader.travelDuration( driverId, tripId )
+    travelLength = dataReader.travelLength( driverId, tripId )
+    distanceOfEndPoint = dataReader.distanceOfEndPoint( driverId, tripId )
+    travelToDistance = travelLength / distanceOfEndPoint
+    
     values = dataReader.speedAccelerationDirectionValues( driverId, tripId )
 
-    #if travelDuration == 0:
-    print( "Travel duration : " + str(travelDuration) )
-    print( values )
+    if len(values) > 1 :
+        ax = fig.add_subplot(331)
+        ax.plot( numpy.arange(0,len(values) ), values[0:,0], 'g-' )
+        ax.set_ylabel('Speed $m/s$')
+        ax.grid(True)
+    
+        ax = fig.add_subplot(332)
+        ax.plot( numpy.arange(0,len(values) ), values[0:,1], 'b-' )
+        ax.set_ylabel('Acceleration $m/s^2$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(333)
+        ax.plot( numpy.arange(0,len(values) ), values[0:,2], 'r-' )
+        ax.set_ylabel('Direction $rad$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(334)
+        ax.plot( values[0:,0], values[0:,1], 'g.', alpha=0.3 )
+        ax.set_xlabel('Speed $m/s$')
+        ax.set_ylabel('Acceleration $m/s^2$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(335)
+        ax.plot( values[0:,0], values[0:,2], 'b.', alpha=0.3 )
+        ax.set_xlabel('Speed $m/s$')
+        ax.set_ylabel('Direction $rad$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(336)
+        ax.plot( values[0:,1], values[0:,2], 'r.', alpha=0.3 )
+        ax.set_xlabel('Acceleration $m/s^2$')
+        ax.set_ylabel('Direction $rad$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(337)
+        ax.hist( values[0:,0], 30, normed=True, facecolor='green' )
+        ax.set_xlabel('Speed $m/s$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(338)
+        ax.hist( values[0:,1], 30, normed=True, facecolor='blue' )
+        ax.set_xlabel('Acceleration $m/s^2$')
+        ax.grid(True)
+
+        ax = fig.add_subplot(339)
+        ax.hist( values[0:,2], 30, normed=True, facecolor='red' )
+        ax.set_xlabel('Direction $rad$')
+        ax.grid(True)
+
+    m,s = divmod( travelDuration, 60)
+    h,m = divmod( m, 60 )
+    text = "Driver : " + str(driverId)
+    fig.text(0.45,0.97, text, horizontalalignment='left')
+    text = "Trip : " + str(tripId)
+    fig.text(0.45,0.94, text, horizontalalignment='left')
+    text = "Travel duration : " + str("%dh %02dm %02ds"%(h,m,s))
+    fig.text(0.05,0.97, text, horizontalalignment='left')
+    text = "Travel length   : " + str(numpy.around(travelLength / 1000.0 , 1 ) ) + " km"
+    fig.text(0.05,0.94, text, horizontalalignment='left')
+    text = "Travel to distance : " + str(numpy.around(travelToDistance , 2 ) )
+    fig.text(0.20,0.94, text, horizontalalignment='left')
+    plt.draw()
+    plt.show(block=False)
 
     r = input()
+    plt.clf()
     if r != '':
         if r[0] == 'q':
             break
         
 dataReader.exit()
 
-plt.close(fig1.number)
+plt.close(fig.number)
